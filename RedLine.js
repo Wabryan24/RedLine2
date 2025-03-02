f3d643455b66cacdfa4945307adbb4f3dd4fe972
Wabryan24 <Wambobryan7@gmail.com>
2025-03-02 16:33:35 +0100

    RedLine

---
+++ RedLine.js
@@ -0,0 +1,22 @@
+    function confirmAge() {
+        localStorage.setItem("ageConfirmed", "true"); // Stocke la confirmation
+        document.getElementById("ageModal").style.display = "none";
+        document.getElementById("content").style.display = "block";
+    }
+
+    function denyAccess() {
+        alert("Désolé, vous n'avez pas l'âge requis.");
+        window.location.href = "https://ecole-it.com/";
+    }
+
+    document.addEventListener("DOMContentLoaded", function () 
+     {
+        if (localStorage.getItem("ageConfirmed") === "true") {
+            document.getElementById("ageModal").style.display = "none";
+            document.getElementById("content").style.display = "block";
+        } else {
+            document.getElementById("ageModal").style.display = "flex";
+            document.getElementById("content").style.display = "none";
+        }
+    });
+
