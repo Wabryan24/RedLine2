869ebdbfaed68cdf686a173895c6c8da1c2d2f81
Wabryan24 <Wambobryan7@gmail.com>
2025-03-02 20:51:40 +0100

    RedLine.html
    A partir d'ici il reste a faire le bloc noir des contacts

--- RedLine.js
+++ RedLine.js
@@ -6,7 +6,7 @@ function confirmAge() {
 
 function denyAccess() {
     alert("Désolé, vous n'avez pas l'âge requis.");
-    window.location.href = "https://ecole-it.com/";
+    window.location.href = "https://youtu.be/fYE6Op49m1Y/";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -19,5 +19,64 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
+
+const movieCards = document.querySelectorAll(".movie-card");
+    movieCards.forEach((card) => {
+      card.addEventListener("mouseenter", function () {
+        this.style.transform = "scale(1.05)";
+        this.style.boxShadow = "0 5px 15px rgba(0,0,0,0.3)";
+        this.style.transition = "all 0.3s ease";
+        const overlay = document.createElement("div");
+        overlay.className = "movie-overlay";
+        overlay.innerHTML = `
+                  <div class="movie-overlay-buttons">
+                      <button class="movie-btn"><i class="fas fa-play"></i></button>
+                      <button class="movie-btn"><i class="fas fa-heart"></i></button>
+                  </div>
+              `;
+        if (!this.querySelector(".movie-overlay")) {
+          this.appendChild(overlay);
+        }
+      });
+      card.addEventListener("mouseleave", function () {
+        this.style.transform = "scale(1)";
+        this.style.boxShadow = "none";
+        const overlay = this.querySelector(".movie-overlay");
+        if (overlay) {
+          overlay.remove();
+        }
+      });
+    });
+
+  
+
+
+    const plusFilmsButtons = document.querySelectorAll(".btn-danger.px-4");
+    plusFilmsButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        alert("Chargement de plus de films");
+      });
+    });
+  
+
+
 /***************************************************************************************************************************/
 
+
+    function loadTrailer(url) {
+        let trailerContainer = document.getElementById('trailerContainer');
+        let trailerFrame = document.getElementById('trailerFrame');
+        
+        trailerFrame.src = url;
+        trailerContainer.classList.add('show');
+    }
+
+    function closeTrailer() {
+        let trailerContainer = document.getElementById('trailerContainer');
+        trailerContainer.classList.remove('show');
+        
+        // Petite code pour éviter que le son continue après fermeture
+        setTimeout(() => {
+            document.getElementById('trailerFrame').src = "";
+        }, 500);
+    }
