596691784340822ef6691b20b2026720fafd6d9f
Wabryan24 <Wambobryan7@gmail.com>
2025-03-02 17:30:59 +0100

    RedLine

--- RedLine.js
+++ RedLine.js
@@ -1,22 +1,23 @@
-    function confirmAge() {
-        localStorage.setItem("ageConfirmed", "true"); // Stocke la confirmation
+function confirmAge() {
+    localStorage.setItem("ageConfirmed", "true"); // Stocke la confirmation
+    document.getElementById("ageModal").style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+
+function denyAccess() {
+    alert("Désolé, vous n'avez pas l'âge requis.");
+    window.location.href = "https://ecole-it.com/";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (localStorage.getItem("ageConfirmed") === "true") {
         document.getElementById("ageModal").style.display = "none";
         document.getElementById("content").style.display = "block";
+    } else {
+        document.getElementById("ageModal").style.display = "flex";
+        document.getElementById("content").style.display = "none";
     }
+});
 
-    function denyAccess() {
-        alert("Désolé, vous n'avez pas l'âge requis.");
-        window.location.href = "https://ecole-it.com/";
-    }
-
-    document.addEventListener("DOMContentLoaded", function () 
-     {
-        if (localStorage.getItem("ageConfirmed") === "true") {
-            document.getElementById("ageModal").style.display = "none";
-            document.getElementById("content").style.display = "block";
-        } else {
-            document.getElementById("ageModal").style.display = "flex";
-            document.getElementById("content").style.display = "none";
-        }
-    });
+/***************************************************************************************************************************/
 
